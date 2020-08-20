e1223de26c26eaf7933664aa5b11afb9d2adb3eb
Alex <aleksander.jordanov@abv.bg>
2020-08-20 16:02:08 +0300

    add stacks and queques

---
+++ StacksAndQueQues/StacksAndQueQues/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StacksAndQueQues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
