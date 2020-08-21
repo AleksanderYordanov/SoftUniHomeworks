5bb7dfcf4cdc12d39e5bb9876791588d15fe95c1
Alex <aleksander.jordanov@abv.bg>
2020-08-21 07:35:25 +0300

    stacks

---
+++ StacksAndQueQues/01FistStack/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01FistStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- StacksAndQueQues/StacksAndQueQues/Program.cs
+++ StacksAndQueQues/StacksAndQueQues/Program.cs
@@ -1,16 +1,26 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace StacksAndQueQues
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string[] thisIsTestForGitHub = new string[5];
-            for (int i = 0; i < thisIsTestForGitHub.Length; i++)
-            {
-                Console.WriteLine("just work please");
-            }
+            var list = new List<string>();
+
+            list.Add("First");
+            list.Add("Second");
+            
+            var myStack = new Stack<string>();
+
+            myStack.Push("Fist");
+            myStack.Push("Leg");
+            myStack.Push("Third");
+
+            var PopItOutTheStack = myStack.Pop();
+            var withoutDeletingLastInStack = myStack.Peek();
+            var contains = myStack.Contains("Five");
+            
         }
     }
 }
