bc57a0ea50048cbf9b6c60179662e08e458e7ba2
Alex <aleksander.jordanov@abv.bg>
2020-08-21 13:27:13 +0300

    add exercise

---
+++ StacksAndQueues/PrintEvenNumbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PrintEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            var nums = new Queue<int>(numbers);
+            var count = 0;
+            while (!nums.All(x => x % 2 == 0))
+            {
+                if (nums.Peek() % 2 == 0)
+                {
+                    Console.Write($"{nums.Dequeue()}, ");
+                    continue;
+                }
+                else
+                {
+                    nums.Dequeue();
+                }
+            }
+            Console.WriteLine(string.Join(", ", nums));
+        }
+    }
+}
---
+++ StacksAndQueues/Supermarket/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Supermarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var clients = new Queue<string>();
+            while (input != "End")
+            {
+                if (input == "Paid")
+                {
+                    for (int i = clients.Count - 1; i >= 0; i--)
+                    {
+                        Console.WriteLine(clients.Dequeue());
+                    }
+                }
+                else
+                {
+                    clients.Enqueue(input);
+                }
+
+                input = Console.ReadLine();
+            }
+            
+            Console.WriteLine($"{clients.Count} people remaining.");
+        }
+    }
+}
